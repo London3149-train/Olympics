acac0dbbd2943f309b4e93a7ed355ea29cbfb84a
LONDON3149\strel <strellook@ya.ru>
2024-01-28 16:15:08 +0300

    Filling classes

---
+++ Olympics/Olympics/Participant.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Olympics
+{
+    public class Participant
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Сountry { get; set; }
+        public string? KindOfSport { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhotoOfAthlete { get; set; }
+        public ParticipantsResult Result { get; set; }
+    }
+}
---
+++ Olympics/Olympics/ParticipantsResult.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Olympics
+{
+    public class ParticipantsResult
+    {
+        public enum Medals : int
+        {
+            Emty = 0,
+            Gold = 1,
+            Silver = 2,
+            Bronze = 3
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Medals Medal { get; set; }
+
+
+    }
+}
